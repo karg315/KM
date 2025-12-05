5a33454e7fd3cb91b135fcf867d4fb73ba14ceab
KevinRodriguez <kevinrogr@unisabana.edu.co>
2025-12-05 18:31:48 -0500

    v3.15 3

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
     return (
         <>     
             <footer>
-                <h5>Kevin "Alejo315" Rodriguez | V3.15.15</h5>
+                <h5>Kevin "Alejo315" Rodriguez | V3.15 2</h5>
             </footer>
         </>
     );
